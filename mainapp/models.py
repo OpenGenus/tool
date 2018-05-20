742b6364fafa4fb6bcc14cf42f94d5eeeb8c4067
suraz <suraznegi@gmail.com>
2018-05-20 13:50:37 +0530

    updated tools model and views

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,12 +5,16 @@ from django.db import models
 
 class Tool(models.Model):
     tool_name = models.CharField(max_length=30)
-    meta_description = models.TextField()
-    meta_title = models.CharField(max_length=30)
-    long_description = models.TextField()
-    short_description = models.TextField()
-    author_name = models.CharField(max_length=30)
-    editors = models.TextField()
-    template_name = models.CharField(max_length=30)
+    url_endpoint = models.CharField(max_length=30,blank=True)
+    meta_description = models.TextField(blank=True)
+    meta_title = models.CharField(max_length=30,blank=True  )
+    long_description = models.TextField(blank=True)
+    short_description = models.TextField(blank=True)
+    author_name = models.CharField(max_length=30,  blank=True)
+    editors = models.TextField(blank=True)
+    template_name = models.CharField(max_length=30,blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.tool_name
