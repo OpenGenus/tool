0b0aad21dce2a4e1301384727e7352f94187c09b
suraz <suraznegi@gmail.com>
2018-05-21 16:56:23 +0530

    added author and tag model

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,3 +3,5 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Tool)
+admin.site.register(models.Author)
+admin.site.register(models.Tag)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    tag = models.CharField(max_length=30)
 
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    website =  models.URLField(max_length=100)
+    discourse = models.URLField(max_length=100)
+    details = models.TextField()
 
 class Tool(models.Model):
     tool_name = models.CharField(max_length=30)
@@ -10,8 +17,9 @@ class Tool(models.Model):
     meta_title = models.CharField(max_length=30,blank=True  )
     long_description = models.TextField(blank=True)
     short_description = models.TextField(blank=True)
-    author_name = models.CharField(max_length=30,  blank=True)
-    editors = models.TextField(blank=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,related_name='author')
+    editors = models.ManyToManyField(Author, related_name='editors',blank=True)
+    tags = models.ManyToManyField(Tag,blank=True)
     template_name = models.CharField(max_length=30,blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
