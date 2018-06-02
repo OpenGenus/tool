158060552199a9720584dea5de6cdd365c9b9ab4
suraz <suraznegi@gmail.com>
2018-06-02 16:08:26 +0530

    added doc to pdf converter

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 
 urlpatterns = [
 url(r'^t/(?P<tool_name>[^/]+)/$',views.tool,name='tool'),
+url(r'^convert_file/$',views.convert_file,name='convert_file'),
 url(r'^$',views.home,name='home'),
 url(r'^u/(?P<user_name>[^/]+)/$',views.user_profile,name='user_profile'),
 url(r'^tags/(?P<tag_name>[^/]+)/$',views.tags,name='tag')
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, reverse, get_object_or_404
 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from . import models
 # Create your views here.
 
@@ -27,3 +27,30 @@ def tags(request,tag_name):
     tools = tag.tool_set.all()
     print(tools)
     return render(request,'general/tags.html',{'tools':tools})
+import os
+import pypandoc
+from django.core.files.storage import default_storage
+from django.conf import settings
+from django.core.files.base import ContentFile
+
+def convert_file(request):
+    if request.method=="POST":
+        file_to_convert = request.FILES.get('file')
+        convert_to = request.POST.get('convert_to')
+
+        filename, ext = os.path.splitext(file_to_convert.name)
+        outputfile_name = '{0}.{1}'.format(filename,convert_to)
+        input_file_path = os.path.join(settings.MEDIA_ROOT,'files',file_to_convert.name)
+        output_file_path= os.path.join(settings.MEDIA_ROOT,'files',outputfile_name)
+        path = default_storage.save(input_file_path,ContentFile(file_to_convert.read()))
+
+        output = pypandoc.convert_file(input_file_path,convert_to,outputfile=output_file_path)
+
+        if os.path.exists(output_file_path):
+            print('exists')
+            with open(output_file_path, 'rb+') as fh:
+                response = HttpResponse(fh.read(), content_type="application/force-download")
+                response['Content-Disposition'] = 'inline; filename=' + os.path.basename(output_file_path)
+                return response
+
+        return HttpResponse('Error while converting', status=404)
