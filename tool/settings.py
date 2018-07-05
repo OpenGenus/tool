559e9182329ffaee75c7488eddca92e4634cd0f9
aimene <aymanemoc1@gmail.com>
2018-07-05 17:40:42 +0200

    binary to decimal converter tool

--- tool/settings.py
+++ tool/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'luyvbu@6mjvtmr9#ch#p)tves=e$$+il_5sz*c_yy=1*ujja9c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 SITE_ID = 2
 
